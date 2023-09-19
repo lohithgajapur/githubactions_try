085b8868b01f869e4432cc561f5038e47153a97b
lohith <lohith725@gmail.com>
2023-09-19 17:09:17 +0530

    update7

--- src/test/java/com/githubactions/cicd_try/SimpleTest.java
+++ src/test/java/com/githubactions/cicd_try/SimpleTest.java
@@ -2,6 +2,8 @@ package com.githubactions.cicd_try;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,8 +17,10 @@ public class SimpleTest {
 	@Test
 	public void firstcicdTest(){
 		WebDriverManager.chromedriver().setup(); 
+		ChromeOptions oc = new ChromeOptions(); 
+		oc.addArguments("--headless");
 		//System.setProperty("webdriver.chrome.driver", "G:\\Java_Software_Latest_version\\2.4 chrome driver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver(); 
+		WebDriver driver = new ChromeDriver(oc); 
 		driver.get("https://google.com/");
 	}
 
