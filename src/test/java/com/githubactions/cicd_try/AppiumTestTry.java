d09ef70120a962bf67663ec324710e90444f0cfd
lohithgajapur <43314280+lohithgajapur@users.noreply.github.com>
2023-09-29 21:16:06 +0530

    Update mn1 AppiumTestTry.java

--- src/test/java/com/githubactions/cicd_try/AppiumTestTry.java
+++ src/test/java/com/githubactions/cicd_try/AppiumTestTry.java
@@ -18,11 +18,12 @@ public class AppiumTestTry {
     capabilities.setCapability("appPackage","com.android.messaging");
     capabilities.setCapability("appActivity","com.android.messaging.ui.conversation.ConversationActivity");
     //capabilities.setCapability("browsername", "chrome");
-    //capabilities.setCapability("automationName", "UiAutomator2");
+    capabilities.setCapability("automationName", "UiAutomator2");
     capabilities.setCapability("avdArgs", "-no-window");
 
     AppiumDriver driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     //driver.get("https://amazon.in/");
+     System.out.println("All most at the end");
     System.out.println("Amazon title: " +driver.getTitle());
     
   }
